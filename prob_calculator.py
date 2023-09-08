8a4d9195613921f5becf0ca2a004caf0130ba04a
AYoonesi <alireza.yoonesi78@gmail.com>
2023-09-08 21:20:35 +0330

    1st attempt

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,42 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        self.contents = []
+        for color, count in kwargs.items():
+            self.contents.extend([color] * count)
 
+    def draw(self, num_balls):
+        drawn_balls = []
+        if num_balls >= len(self.contents):
+            return self.contents
+        for _ in range(num_balls):
+            ball_index = random.randint(0, len(self.contents) - 1)
+            drawn_balls.append(self.contents.pop(ball_index))
+        return drawn_balls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    count_successful_experiments = 0
+
+    for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        drawn_balls = hat_copy.draw(num_balls_drawn)
+        drawn_balls_dict = {}
+        
+        for ball in drawn_balls:
+            if ball in drawn_balls_dict:
+                drawn_balls_dict[ball] += 1
+            else:
+                drawn_balls_dict[ball] = 1
+
+        successful_experiment = True
+        for color, count in expected_balls.items():
+            if color not in drawn_balls_dict or drawn_balls_dict[color] < count:
+                successful_experiment = False
+                break
+        
+        if successful_experiment:
+            count_successful_experiments += 1
+
+    return count_successful_experiments / num_experiments
